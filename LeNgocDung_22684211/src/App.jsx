d9e8aedfb7c555af61b365906093c2b480fae34e
Co Phuong Hang <Dungheongunhubo>
2025-02-19 10:37:16 +0700

    Finished BaiTap01

---
+++ LeNgocDung_22684211/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import BaiTap01 from './components/BaiTap01'
+
+function App() {
+  
+  return (
+    <>
+      <BaiTap01></BaiTap01>
+    </>
+  )
+}
+
+export default App
---
+++ LeNgocDung_22684211/src/components/BaiTap01.jsx
@@ -0,0 +1,25 @@
+import { useState } from 'react'
+import './BaiTap01.css';
+export default function BaiTap01() {
+    const [name, setName] = useState('')
+    const [text, setText] = useState('') 
+    function handleClick() {
+        setText(name)
+    }
+    function handleChange(e) {
+        setName(e.target.value)
+    }
+
+    return (
+        <>
+            <input onChange={handleChange} type="text" 
+            />
+            <br />
+            <br />
+
+            <button onClick={handleClick}>Click me</button>
+            <br />
+            <span>{text}</span>
+        </>
+    )
+}
